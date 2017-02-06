a6cdf8a5ce61004132b3197dfcfae2ebf5ab02b2
Masaki Ozawa <ozamasa@shiolab.com>
2017-02-06 21:21:07 +0900

    Update camera.sh

--- camera.sh
+++ camera.sh
@@ -5,5 +5,5 @@ Y=$(date +"%Y")
 M=$(date +"%m")
 D=$(date +"%d")
 DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill -w 1920 -h 1080 -o /home/pi/camera/img/$DATE.jpg
+raspistill -w 1920 -h 1080 -o /home/pi/raspberry/camera/img/$DATE.jpg
 sudo aws s3 mv /home/pi/raspberry/camera/img/$DATE.jpg s3://s3.shiolab.com/camera/sip/$Y/$M/$D/
