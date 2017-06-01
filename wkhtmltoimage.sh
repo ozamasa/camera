9738577af9fa519f1eaeca3019cb2ac385c85af4
Masaki Ozawa <ozamasa@shiolab.com>
2017-06-01 16:59:13 +0900

    Update wkhtmltoimage.sh

--- wkhtmltoimage.sh
+++ wkhtmltoimage.sh
@@ -3,5 +3,5 @@ Y=$(date +"%Y")
 M=$(date +"%m")
 D=$(date +"%d")
 DATE=$(date +"%Y-%m-%d_%H%M")
-xvfb-run wkhtmltoimage --crop-h 540 --crop-w 740 --crop-x 30 --crop-y 52 http://live.shiolab.com /home/pi/camera/plot/$DATE.jpg
-sudo aws s3 mv /home/pi/camera/plot/$DATE.jpg s3://s3.shiolab.com/sensor/shiolab/$Y/$M/$D/
+xvfb-run wkhtmltoimage --crop-h 540 --crop-w 740 --crop-x 30 --crop-y 52 http://live.shiolab.com /home/pi/raspberry/plot/$DATE.jpg
+sudo aws s3 mv /home/pi/raspberry/plot/$DATE.jpg s3://s3.shiolab.com/sensor/shiolab/$Y/$M/$D/
